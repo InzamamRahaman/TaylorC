93addb916600518259d34417eef744faf6a17224
Inzamam Rahaman <inzamamrahaman@Inzamams-MacBook-Pro.local>
2023-10-22 22:25:45 -0400

    main language implemented

--- src/grammar.py
+++ src/grammar.py
@@ -85,6 +85,8 @@ class TaylorCTranspilerVisitor(PTNodeVisitor):
         #return children
         return '\n'.join(map(lambda x: f'    {x}', children))
 
+
+
     def visit_if_stmnt(self, node, children):
         body = '\n'.join(map(lambda x: f'    {x}', children[1].split('\n')))
         res = f'if {children[0]}:\n'
@@ -118,7 +120,31 @@ class TaylorCTranspilerVisitor(PTNodeVisitor):
         return ', '.join(children)
 
     def visit_argument(self, node, children):
-        return children
+        return str(children[0])
+
+    def visit_return_stmnt(self, node, children):
+        return 'return ' + children[0]
+
+
+    def visit_function_body(self, node, children):
+        res = 'pass'
+        if len(children) > 0:
+            res = '\n'.join(map(lambda x: f'{x}', children))
+        return res
+
+    def visit_void_function(self, node, children):
+        function_name = children[0]
+        header = f'def {function_name}():\n'
+        body = '    pass'
+        if len(children) > 1:
+            processed_children = map(lambda x: x.split('\n'), children[1:])
+            processed_children = [item for sublist in processed_children for item in sublist]
+            #print(processed_children)
+            body = map(lambda x: f'    {x}', processed_children)
+            body = '\n'.join(body)
+        func_def = f'{header}\n{body}'
+        return func_def
+
 
     def visit_non_void_function(self, node, children):
         argument_listing = children[1]
@@ -126,26 +152,36 @@ class TaylorCTranspilerVisitor(PTNodeVisitor):
         header = f'def {function_name}({argument_listing}):\n'
         body = '    pass'
         if len(children) > 2:
-            body = map(lambda x: f'    {x}', children[2:])
+            processed_children = map(lambda x: x.split('\n'), children[2:])
+            processed_children = [item for sublist in processed_children for item in sublist]
+            body = map(lambda x: f'    {x}', processed_children)
             body = '\n'.join(body)
         func_def = f'{header}\n{body}'
         return func_def
 
-    def visit_void_function(self, node, children):
-        function_name = children[0]
-        header = f'def {function_name}():\n'
-        body = '    pass'
+    def visit_function_invoke(self, node, children):
+        arguments = []
         if len(children) > 1:
-            body = map(lambda x: f'    {x}', children[1:])
-            body = '\n'.join(body)
-        func_def = f'{header}\n{body}'
-        return func_def
+            for child in children[1:]:
+                arguments.append(child)
+        res = f'{children[0]}({",".join(arguments)})'
+        return res
+
+    def visit_function_invoke_with_return(self, node, children):
+        arguments = []
+        if len(children) > 2:
+            for child in children[1:-1]:
+                arguments.append(child)
+        res = f'{children[-1]} = {children[0]}({",".join(arguments)})'
+        return res
+
+
 
 
 
 def start(): return "ARE WE OUT OF THE WOODS"
 def end(): return "ALL TOO WELL TIME FLIES"
-def variable_name(): return _(r'[a-zA-Z_][a-zA-Z0-9]*')
+def variable_name(): return _(r'[a-z][a-zA-Z0-9]*')
 def string_literal(): return _(r'"(.*)"')
 def int_literal(): return _(r'(-?[1-9][0-9]*)|0')
 def float_literal(): return _(r'((-?[1-9][0-9]*)|0)\.[0-9]+')
@@ -205,19 +241,36 @@ def variable_assignment():
     return 'BEGIN AGAIN, STARTING OVER', variable_name, \
         'ONCE UPON A TIME, A FEW MISTAKES AGO', number_literal, ZeroOrMore(operation), \
         "AND THE STORY'S GOT DUST ON EVERY PAGE"
-def statement(): return [print_statement, variable_creation, variable_assignment, if_stmnt, if_else_stmnt, while_block]
+
+def statement(): return [print_statement, variable_creation,
+                         variable_assignment, if_stmnt, if_else_stmnt,
+                         while_block, function_invoke_with_return, function_invoke]
+def return_stmnt():
+    return "I'LL COME BACK STRONGER THAN A 90'S TREND", [variable_name, literal]
+
+def function_body():
+    return ZeroOrMore(statement)
 
 def void_function():
-    return "ARE YOU READY FOR IT?", variable_name, ZeroOrMore(statement), 'WE ARE NEVER EVER GETTING BACK TOGETHER'
+    return "ARE YOU READY FOR IT?", variable_name, function_body, \
+        Optional(return_stmnt), 'WE ARE NEVER EVER GETTING BACK TOGETHER'
 
 def non_void_function():
-    return "ARE YOU READY FOR IT?", variable_name, argument_list, ZeroOrMore(statement), 'WE ARE NEVER EVER GETTING BACK TOGETHER'
+    return "ARE YOU READY FOR IT?", variable_name, argument_list, function_body, \
+        return_stmnt, 'WE ARE NEVER EVER GETTING BACK TOGETHER'
 
 def function():
     return [non_void_function, void_function]
 
 def body_elems():
-    return [statement, if_else_stmnt, if_stmnt, while_block, function]
+    return [statement, function]
+
+def function_invoke():
+    return 'JUST SAY YES', variable_name, ZeroOrMore([variable_name, literal])
+
+def function_invoke_with_return():
+    return 'JUST SAY YES', variable_name, ZeroOrMore([variable_name, literal]), \
+        'EVERYTHING HAS CHANGED', variable_name
 
 def body(): return [ZeroOrMore(body_elems), '']
 
@@ -260,21 +313,25 @@ BUT WE HAD TO END IT
 JUST TAKE A DEEP BREATH AND LET IT GO
 ARE YOU READY FOR IT? sayhello
 SPEAK NOW OR FOREVER HOLD YOUR PEACE "Hello"
+IF THE HIGH WAS WORTH THE PAIN 1
+BLANK SPACE, WRITE YOUR NAME bar
+IT'S A NEW SOUNDTRACK 23
+BUT DREAMS COME TRUE
+BLANK SPACE, WRITE YOUR NAME bar
+IT'S A NEW SOUNDTRACK 12
+BUT WE HAD TO END IT
+I'LL COME BACK STRONGER THAN A 90'S TREND bar
 WE ARE NEVER EVER GETTING BACK TOGETHER
+ARE YOU READY FOR IT? test1
+ALL I KNOW IS THAT YOU'RE MINE bar
+BLANK SPACE, WRITE YOUR NAME bar
+IT'S A NEW SOUNDTRACK 23
+I'LL COME BACK STRONGER THAN A 90'S TREND bar
+WE ARE NEVER EVER GETTING BACK TOGETHER
+JUST SAY YES foo bar
+EVERYTHING HAS CHANGED food
 ALL TOO WELL TIME FLIES
 """
-
-#"ARE YOU READY FOR IT?", variable_name, ZeroOrMore(statement), 'WE ARE NEVER EVER GETTING BACK TOGETHER'
-
-#'IF THE HIGH WAS WORTH THE PAIN', operand, if_body, 'BUT WE HAD TO END IT'
-
-# parser_ind = ParserPython(number_literal)
-# parse_tree = parser_ind.parse('2')
-# print(parse_tree)
-#
-# parser_ind = ParserPython(decrement)
-# parse_tree = parser_ind.parse('SHAKE IT OFF, SHAKE IT OFF 2')
-# print(parse_tree)
 
 parser = ParserPython(program)
 parse_tree = parser.parse(test.strip())
